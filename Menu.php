0d8300b408795990d7c1e41f9934b4876b14d7a6
Judith soro Corvo <judithsorocorvo@alumno.ieselrincon.es>
2023-10-30 12:02:05 +0000

    subir

--- Menu.php
+++ Menu.php
@@ -8,61 +8,55 @@
 <body>
 
 <style>
-    body {
-            font-family: Arial, sans-serif;
-            background-color: #f0f0f0;
-        }
-        h1 {
-            text-align: center;
-        }
-        form {
-            max-width: 400px;
-            margin: 0 auto;
-            background-color: #fff;
-            padding: 20px;
-            border-radius: 5px;
-            box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
-        }
-        label {
-            display: block;
-            margin-bottom: 5px;
-        }
-        input[type="text"], input[type="file"] {
-            width: 100%;
-            padding: 10px;
-            margin-bottom: 10px;
-            border: 1px solid #ccc;
-            border-radius: 3px;
-        }
-        input[type="submit"] {
-            background-color: #0074d9;
-            color: #fff;
-            padding: 10px 20px;
-            border: none;
-            border-radius: 3px;
-            cursor: pointer;
-        }
-        input[type="submit"]:hover {
-            background-color: #0056b3;
-        }
+
+
+#menu {
+  background-color: #333;
+  padding: 10px;
+}
+
+#menu ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+#menu li {
+  margin-bottom: 10px;
+}
+
+#menu a {
+  text-decoration: none;
+  color: #fff;
+  font-weight: bold;
+  display: block;
+  padding: 5px;
+}
+
+#menu a:hover {
+  background-color: #007BFF;
+}
+
     </style>
 
-    
-</head>
-<body>
-    <h1>Crear Producto</h1>
-    <form>
-        <label for="nombre">Nombre:</label>
-        <input type="text" name="nombre" id="nombre">
-        <label for="precio">Precio:</label>
-        <input type="text" name="precio" id="precio">
-        <label for="imagen">Imagen:</label>
-        <input type="file" name="imagen" id="imagen">
-        <label for="categoria">Categoría:</label>
-        <input type="text" name="categoria" id="categoria">
-        <input type="submit" value="Enviar">
-    </form>
+
+
+
+<div id="menu">
    
+     <ul>
+        
+        <li><a href="crearProducto.php">Crear producto</a></li>
+        <li><a href="#">Consultar el listado de productos</a></li>
+        <li><a href="#">Modificar producto</a></li>
+        <li><a href="#">Eliminar producto</a></li>
+      
+
+    </ul>
+
+</div>
+
+   
+
 
 </body>
 </html>
