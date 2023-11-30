5a9b351ae60e49b18d6f8d0bae30a269492834ef
Judith soro Corvo <judithsorocorvo@alumno.ieselrincon.es>
2023-11-30 13:04:43 +0000

    aaaaaaaaa

--- form.php
+++ form.php
@@ -13,25 +13,28 @@ try {
     $conn = new PDO("mysql:host=$servername;dbname=$bd", $username, $password);
     // set the PDO error mode to exception
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    $sentencia= $conn->prepare("select contrasena_hash from usuarios2 ");
+    $sentencia= $conn->prepare("select contrasena_hash from usuarios2 where correo_electronico like '".$_POST['correo']."'");
     $sentencia->execute();
     $fila=$sentencia->fetch(PDO::FETCH_ASSOC);
   
+   
+
     if(password_verify( $_POST['pass'],  $fila['contrasena_hash']))
     {
 
-    session_start();
-    $_SESSION["usuario"]=$_POST['correo'] ;
+      session_start();
+       $_SESSION["usuario"]=$_POST['correo'] ;
         echo $_SESSION["usuario"];
         header("Location:Menu.php");
+      exit();
 
-
-    } else {
+       } else {
         header("Location:id.php");
         }
-  } catch(PDOException $e) {
+
+    } catch(PDOException $e) {
     echo "<br> Connection failed: " . $e->getMessage();
-  }
+    }
      
 
  
